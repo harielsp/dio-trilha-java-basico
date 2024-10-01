0cc3d9eb2becfbbd309df84d2a8596cce6461832
harielsp <harielsp@gmail.com>
2024-09-30 23:19:28 -0300

    20240930

---
+++ DocumentacaoJAVA/src/Comenrarios.java
@@ -0,0 +1,11 @@
+public class Comenrarios {
+    public static void main(String[] args) {
+        //Comentario de 1 linha somente
+
+        /*Comentario multi linhas .... somente um teste
+         * para saber como se comporta
+         */
+    
+        }
+}
+/**documentação .... sempre fora do bloco */
